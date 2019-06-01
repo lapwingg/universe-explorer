c0542ba80e3500253906d28ebe726d7b100350a8
Lapwingg <kamilczajka2@gmail.com>
2019-06-01 18:30:53 +0200

    Creating URL Request - some amendments

--- asteroids/ViewController.swift
+++ asteroids/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+// TO STRUCT
 struct NASA: Codable {
     let date: String
     let explanation: String
@@ -25,28 +26,40 @@ class ViewController: UIViewController {
     }
 
     private func startDownload() {
-        guard let url = URL(string: "https://api.nasa.gov/planetary/apod?api_key=K2O45BgzWeaeHQUueqOSeVCa95d2nckyadRYC8IL") else {
-            fatalError("Invalid URL!")
+        // REQUEST FACTORY?
+        var requestBuilder = URLComponents(string: "https://api.nasa.gov/planetary/apod")!
+        requestBuilder.queryItems = [
+            URLQueryItem(name: "api_key", value: "K2O45BgzWeaeHQUueqOSeVCa95d2nckyadRYC8IL"),
+            URLQueryItem(name: "date", value: "2019-05-16"),
+            URLQueryItem(name: "hd", value: "true")
+        ]
+        guard let requestURL = requestBuilder.url else {
+            fatalError("UNABLE TO GET URL FOR \(requestBuilder)")
         }
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "GET"
+        
+        // PROVIDER CLASS?
+        let downloadData = URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
                 fatalError("ERROR! + \(error)")
             }
-            guard let httpResponse = response as? HTTPURLResponse,
-                (200...299).contains(httpResponse.statusCode) else {
-                    fatalError("ERROR -> STATUS")
+            // 429 TOO MANY RESPONSE !!!
+            guard let httpStatus = response as? HTTPURLResponse, 200...299 ~= httpStatus.statusCode else {
+                fatalError("statusCode should be 200 - 299")
             }
             guard let data = data else {
                 fatalError("NO DATA")
             }
+            // DECODER CLASS?
             let json = try? JSONDecoder().decode(NASA.self, from: data)
             DispatchQueue.main.async {
                 self.resultField.text = "\(json?.date)\n\(json?.explanation)\n\(json?.hdurl)\n\(json?.media_type)\n\(json?.service_version)\n\(json?.title)\n\(json?.url)"
             }
         }
-        task.resume()
+        downloadData.resume()
     }
-    
+    // PRESENTER?
     @IBAction func downloadButton(_ sender: Any) {
         startDownload()
     }
