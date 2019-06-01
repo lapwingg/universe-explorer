3789b54906be9dd98135c9872fd974e5c0773cf3
Lapwingg <kamilczajka2@gmail.com>
2019-06-01 16:27:56 +0200

    Decoding sample JSON object and printing on screen

--- asteroids/ViewController.swift
+++ asteroids/ViewController.swift
@@ -7,6 +7,16 @@
 
 import UIKit
 
+struct NASA: Codable {
+    let date: String
+    let explanation: String
+    let hdurl: String
+    let media_type: String
+    let service_version: String
+    let title: String
+    let url: String
+}
+
 class ViewController: UIViewController {
     @IBOutlet weak var resultField: UITextView!
     
@@ -19,23 +29,19 @@ class ViewController: UIViewController {
             fatalError("Invalid URL!")
         }
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            do {
-                if let error = error {
-                    fatalError("ERROR! + \(error)")
-                }
-                guard let httpResponse = response as? HTTPURLResponse,
-                    (200...299).contains(httpResponse.statusCode) else {
-                        fatalError("ERROR -> STATUS")
-                }
-                guard let data = data else {
-                    fatalError("NO DATA")
-                }
-                guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary else {
-                    fatalError("CONVERSION ERROR")
-                }
-                print(json)
-            } catch let error as NSError {
-                print(error.debugDescription)
+            if let error = error {
+                fatalError("ERROR! + \(error)")
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                (200...299).contains(httpResponse.statusCode) else {
+                    fatalError("ERROR -> STATUS")
+            }
+            guard let data = data else {
+                fatalError("NO DATA")
+            }
+            let json = try? JSONDecoder().decode(NASA.self, from: data)
+            DispatchQueue.main.async {
+                self.resultField.text = "\(json?.date)\n\(json?.explanation)\n\(json?.hdurl)\n\(json?.media_type)\n\(json?.service_version)\n\(json?.title)\n\(json?.url)"
             }
         }
         task.resume()
