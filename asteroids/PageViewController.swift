4e2dba43bedfa18877f29ddd13f4736a112ecc48
Lapwingg <kamilczajka2@gmail.com>
2019-06-08 18:21:03 +0200

    PageViewController improvments

--- asteroids/PageViewController.swift
+++ asteroids/PageViewController.swift
@@ -7,23 +7,76 @@
 
 import UIKit
 
-class PageViewController: UIPageViewController {
-
+internal class PageViewController: UIPageViewController {
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [newColoredViewController(name: "PageView1"),
+                newColoredViewController(name: "PageView2")]
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        dataSource = self
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
 
-        // Do any additional setup after loading the view.
+    private func newColoredViewController(name: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: name)
+    }
+}
+
+extension PageViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
     }
-    */
-
+    
+//    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+//        return orderedViewControllers.count
+//    }
+//
+//    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+//        guard let firstViewController = viewControllers?.first,
+//            let firstViewControllerIndex = orderedViewControllers.firstIndex(of: firstViewController) else {
+//                return 0
+//        }
+//
+//        return firstViewControllerIndex
+//    }
 }
