6e5e635d4b3ac209d2d31e832832fb0b04b47253
Lapwingg <kamilczajka2@gmail.com>
2019-06-09 12:12:16 +0200

    Added popover for search bar button item

--- asteroids/PageViewController.swift
+++ asteroids/PageViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-internal class PageViewController: UIPageViewController {
+internal class PageViewController: UIPageViewController, UIPopoverPresentationControllerDelegate {
     @IBOutlet weak var searchByDate: UIBarButtonItem!
     @IBOutlet weak var addToFavourite: UIBarButtonItem!
     
@@ -32,12 +32,35 @@ internal class PageViewController: UIPageViewController {
         print("Add To Favourite Was Tapped")
     }
     
-    @IBAction func searchByDateTapped(_ sender: Any) {
+    @IBAction func searchByDateTapped(_ sender: UIBarButtonItem) {
         print("Search By Date Was Tapped")
+        let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let popoverContent = storyboard.instantiateViewController(withIdentifier: "SearchByDatePopover")
+        let nav = UINavigationController(rootViewController: popoverContent)
+        nav.modalPresentationStyle = .popover
+        let popover = nav.popoverPresentationController
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            popoverContent.preferredContentSize = CGSize(width: 500, height: 500)
+        } else {
+            popoverContent.preferredContentSize = CGSize(width: 250, height: 250)
+        }
+      
+        popover!.delegate = self
+        popover!.sourceView = self.view
+        popover!.sourceRect = CGRect(x: 0, y: 0, width: 100, height: 100)
+        self.present(nav, animated: true, completion: nil)
     }
     
     private func newColoredViewController(name: String) -> UIViewController {
         return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: name)
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        return adaptivePresentationStyle(for: controller)
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
     }
 }
 
